f97595c12fa6c5b67547ea0893baa423d4fc9715
jaylordbubucayu <57398270+jaylordbubucayu@users.noreply.github.com>
2022-03-24 16:44:20 +0800

    add the navbar for mobile

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+/*NAV BAR */
+
+
+    const menu = document.getElementById('nav-menu');
+    const sideBar = document.getElementsByClassName('nav-mobile-sidebar-container');
+    
+
+    menu.addEventListener('click', ()=>{
+      sideBar[0].classList.add('show');
+      sideBar[0].childNodes[1].classList.add('show-translate');
+      sideBar[0].childNodes[3].classList.add('show-translate');
+    });
+
+
+
+    const close = document.getElementById('menu-close');
+
+
+    close.addEventListener('click', ()=> {
+
+        sideBar[0].classList.remove('show');
+        sideBar[0].childNodes[1].classList.remove('show-translate');
+        sideBar[0].childNodes[3].classList.remove('show-translate');
+    });
