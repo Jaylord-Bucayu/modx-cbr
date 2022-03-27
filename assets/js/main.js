aa6700b74baeb7012cc88ebb3580b2ca86fff366
jaylordbubucayu <57398270+jaylordbubucayu@users.noreply.github.com>
2022-03-27 10:22:45 +0800

    Added culture page

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,16 @@
 
     const menu = document.getElementById('nav-menu');
     const sideBar = document.getElementsByClassName('nav-mobile-sidebar-container');
+    const body = document.querySelector('body');
     
 
     menu.addEventListener('click', ()=>{
       sideBar[0].classList.add('show');
       sideBar[0].childNodes[1].classList.add('show-translate');
       sideBar[0].childNodes[3].classList.add('show-translate');
+        
+      body.style.overflow = "hidden";
+
     });
 
 
@@ -21,4 +25,5 @@
         sideBar[0].classList.remove('show');
         sideBar[0].childNodes[1].classList.remove('show-translate');
         sideBar[0].childNodes[3].classList.remove('show-translate');
+        body.style.overflow = "auto";
     });
